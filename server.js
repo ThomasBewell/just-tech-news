69109668f8922462f79939f407a674d00262be79
Thomas Bewell <tombewell@gmail.com>
2021-09-02 14:49:34 -0500

    conditionally render login and logout links

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ const sess = {
     })
 };
 
-app.use(session(sess));
-
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
